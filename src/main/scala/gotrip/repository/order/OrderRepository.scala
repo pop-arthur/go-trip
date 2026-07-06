1ca6023373c20537970866b309a6536a7e3a25b5
arthur <artu.200505@gmail.com>
2026-07-06 09:16:00 +0300

    refactor(order): pull out order status sync service

--- src/main/scala/gotrip/repository/order/OrderRepository.scala
+++ src/main/scala/gotrip/repository/order/OrderRepository.scala
@@ -10,6 +10,7 @@ import skunk.Session
 
 trait OrderRepository[F[_]]:
   def listByTrip(userId: UserId, tripId: TripId, params: OrderSearchParams): F[List[Order]]
+  def listExternalByUser(userId: UserId): F[List[Order]]
   def findById(orderId: OrderId): F[Option[Order]]
   def findByUser(userId: UserId, orderId: OrderId): F[Option[Order]]
   def create(order: Order): F[Order]
